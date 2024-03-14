599df9aa370b64522a076f39dcf0e2fb549b3237
Anas Mullappally <101805320+anasmullappally@users.noreply.github.com>
2024-03-15 00:22:39 +0530

    order success design changes

--- src/component/OrderSuccess/index.js
+++ src/component/OrderSuccess/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 // import Img
-import img1 from '../../assets/img/common/delivery_success.png';
+// import img1 from '../../assets/img/common/delivery_success.png';
 import success from '../../assets/img/email/success.png';
 import ofd from '../../assets/img/email/order-out-delivery.png';
 import shipped from '../../assets/img/email/order-shipped.png';
@@ -9,7 +9,7 @@ import orderDelivered from '../../assets/img/email/order-delivery.png';
 
 // import pro1 from '../../assets/img/email/pro-3.jpg';
 // import pro2 from '../../assets/img/email/pro-5.jpg';
-import spoce from '../../assets/img/email/space.jpg';
+// import spoce from '../../assets/img/email/space.jpg';
 import { singleOrder } from '../../app/data/productsData';
 
 const OrderSuccess = () => {
@@ -18,347 +18,6 @@ const OrderSuccess = () => {
     history.goBack();
   };
   return (
-    // <>
-    //   <div className='tables_area'>
-    //     <div className='back_btn_emial'>
-    //       <button className='theme-btn-one btn-black-overlay btn_sm' onClick={routeChange}>
-    //         <i className='fa fa-arrow-left mr-2'></i>Go Back
-    //       </button>
-    //     </div>
-    //     <div
-    //       className='box_table '
-    //     // style={{
-    //     //   padding: '0 30px',
-    //     //   BackgroundColor: 'red',
-    //     //   BoxShadow: ' 0px 0px 14px -4px rgba(0, 0, 0, 0.2705882353)',
-    //     //   width: '100%',
-    //     //   display: 'block',
-    //     // }}
-    //     >
-    //       <div>
-    //         <tr>
-    //           <td>
-    //             <table align='center' border='0' cellPadding='0' cellSpacing='0'>
-    //               <tbody>
-    //                 <tr>
-    //                   <td>
-    //                     <img src={img1} alt='img' style={{ marginBottom: '30px' }} />
-    //                   </td>
-    //                 </tr>
-    //                 <tr>
-    //                   <td>
-    //                     <img src={success} alt='img' />
-    //                   </td>
-    //                 </tr>
-    //                 <tr>
-    //                   <td>
-    //                     <h2 className='title'>thank you</h2>
-    //                   </td>
-    //                 </tr>
-    //                 <tr>
-    //                   <td>
-    //                     <p>Payment Is Successfully Processed And Your Order Is On The Way</p>
-    //                     <p>Transaction ID:{singleOrder.payment._id}</p>
-    //                   </td>
-    //                 </tr>
-    //                 {/* <tr></tr> */}
-    //                 {/* <tr>
-    //                   <td>
-    //                     {singleOrder.status === "shipped" && <img src={shipped} alt='img' style={{ marginTop: '30px', marginBottom: '30px' }} />}
-    //                     {singleOrder.status === "delivered" && <img src={orderDelivered} alt='img' style={{ marginTop: '30px', marginBottom: '30px' }} />}
-    //                     {singleOrder.status === "ofd" && <img src={ofd} alt='img' style={{ marginTop: '30px', marginBottom: '30px' }} />}
-    //                   </td>
-    //                 </tr> */}
-    // {singleOrder.products.map((item) => (
-    //   <>
-    //     <tr >
-    //       <th> {item.product.title}  </th>
-    //     </tr>
-    //     <tr >
-    //       <td>
-    //         {item.orderStatus === "shipped" && <img src={shipped} alt='img' style={{ marginTop: '30px', marginBottom: '30px' }} />}
-    //         {item.orderStatus === "delivered" && <img src={orderDelivered} alt='img' style={{ marginTop: '30px', marginBottom: '30px' }} />}
-    //         {item.orderStatus === "ofd" && <img src={ofd} alt='img' style={{ marginTop: '30px', marginBottom: '30px' }} />}
-    //       </td>
-    //     </tr>
-    //   </>
-    // ))}
-    //               </tbody>
-    //             </table>
-    //             <table border='0' cellPadding='0' cellSpacing='0' className='mt-4'>
-    //               <tbody>
-    //                 <tr>
-    //                   <td>
-    //                     <h2 className='title'>YOUR ORDER DETAILS</h2>
-    //                   </td>
-    //                 </tr>
-    //               </tbody>
-    //             </table>
-    //             <table className='order-detail' border='0' cellPadding='0' cellSpacing='0' align='left'>
-    //               <tbody>
-    //                 <tr align='left'>
-    //                   <th>PRODUCT</th>
-    //                   <th style={{ paddingLeft: ' 15px' }}>DESCRIPTION</th>
-    //                   <th>QUANTITY</th>
-    //                   <th>PRICE</th>
-    //                 </tr>
-    //                 {singleOrder.products.map((item, index) => (
-    //                   <tr key={item.product._id}>
-    //                     <td>
-    //                       <img src={item.product.img} alt='img' width='70' />
-    //                     </td>
-    //                     <td valign='top' style={{ paddingLeft: '15px' }}>
-    //                       <h5 style={{ marginTop: '15px' }}>{item.product.title}</h5>
-    //                     </td>
-    //                     <td valign='top' style={{ paddingLeft: '15px' }}>
-    //                       <h5 style={{ fontSize: '14px', color: '#444', marginTop: '15px', marginBottom: ' 0px' }}>
-    //                         Size :<span> L</span>{' '}
-    //                       </h5>
-    //                       <h5 style={{ fontSize: '14px', color: '#444', marginTop: '10px' }}>
-    //                         QTY : <span>{item.product.quantity}</span>
-    //                       </h5>
-    //                     </td>
-    //                     <td valign='top' style={{ paddingLeft: '15px' }}>
-    //                       <h5 style={{ fontSize: '14px', Color: '#444', marginTop: '15px' }}>
-    //                         <b>&#8377; {item.product.price}</b>
-    //                       </h5>
-    //                     </td>
-    //                   </tr>
-
-    //                 ))}
-    //                 {/* <tr>
-    //                   <td>
-    //                     <img src={pro2} alt='img' width='70' />
-    //                   </td>
-    //                   <td valign='top' style={{ paddingLeft: '15px' }}>
-    //                     <h5 style={{ marginTop: '15px' }}>Three seater Wood Style for Badroom </h5>
-    //                   </td>
-    //                   <td valign='top' style={{ paddingLeft: '15px' }}>
-    //                     <h5 style={{ fontSize: '14px', color: '#444', marginTop: '15px', marginBottom: '0px' }}>
-    //                       Size :<span> XL</span>{' '}
-    //                     </h5>
-    //                     <h5 style={{ fontSize: '14px', color: '#444', marginTop: '10px' }}>
-    //                       QTY : <span>1</span>
-    //                     </h5>
-    //                   </td>
-    //                   <td valign='top' style={{ paddingLeft: '15px' }}>
-    //                     <h5 style={{ fontSize: '14px', Color: '#444', marginTop: '15px' }}>
-    //                       <b>&#8377; {singleOrder.products[1].product.price}</b>
-    //                     </h5>
-    //                   </td>
-    //                 </tr> */}
-    //                 <tr>
-    //                   <td
-    //                     colSpan='2'
-    //                     style={{
-    //                       lineHeight: '49px',
-    //                       fontSize: '13px',
-    //                       color: '#000000',
-    //                       paddingLeft: '20px',
-    //                       textAlign: 'left',
-    //                       borderRight: ' unset',
-    //                     }}
-    //                   >
-    //                     Products:
-    //                   </td>
-    //                   <td
-    //                     colSpan='3'
-    //                     className='price'
-    //                     style={{
-    //                       lineHeight: '49px',
-    //                       textAlign: 'right',
-    //                       paddingRight: '28px',
-    //                       fontSize: '13px',
-    //                       color: '#000000',
-    //                       TextAlign: 'right',
-    //                       borderLeft: 'unset',
-    //                     }}
-    //                   >
-    //                     <b>&#8377; {singleOrder.total}</b>
-    //                   </td>
-    //                 </tr>
-    //                 <tr>
-    //                   <td
-    //                     colSpan='2'
-    //                     style={{
-    //                       lineHeight: '49px',
-    //                       fontSize: '13px',
-    //                       color: '#000000',
-    //                       paddingLeft: '20px',
-    //                       textAlign: 'left',
-    //                       borderRight: ' unset',
-    //                     }}
-    //                   >
-    //                     Discount :
-    //                   </td>
-    //                   <td
-    //                     colSpan='3'
-    //                     className='price'
-    //                     style={{
-    //                       lineHeight: '49px',
-    //                       textAlign: 'right',
-    //                       paddingRight: '28px',
-    //                       fontSize: '13px',
-    //                       color: '#000000',
-    //                       TextAlign: 'right',
-    //                       borderLeft: 'unset',
-    //                     }}
-    //                   >
-    //                     <b>&#8377;{singleOrder.discount}</b>
-    //                   </td>
-    //                 </tr>
-    //                 <tr>
-    //                   <td
-    //                     colSpan='2'
-    //                     style={{
-    //                       lineHeight: '49px',
-    //                       fontFamily: ' Arial',
-    //                       fontSize: '13px',
-    //                       color: '#000000',
-    //                       paddingLeft: '20px',
-    //                       textAlign: 'left',
-    //                       borderRight: 'unset',
-    //                     }}
-    //                   >
-    //                     Gift Wrapping:{' '}
-    //                   </td>
-    //                   <td
-    //                     colSpan='3'
-    //                     className='price'
-    //                     style={{
-    //                       lineHeight: '49px',
-    //                       textAlign: 'right',
-    //                       paddingRight: '28px',
-    //                       fontSize: '13px',
-    //                       color: '#000000',
-    //                       TextAlign: 'right',
-    //                       borderLeft: 'unset',
-    //                     }}
-    //                   >
-    //                     <b>&#8377; {singleOrder.wrapping}</b>
-    //                   </td>
-    //                 </tr>
-    //                 <tr>
-    //                   <td
-    //                     colSpan='2'
-    //                     style={{
-    //                       lineHeight: '49px',
-    //                       fontSize: '13px',
-    //                       color: '#000000',
-    //                       paddingLeft: '20px',
-    //                       textAlign: 'left',
-    //                       borderRight: 'unset',
-    //                     }}
-    //                   >
-    //                     Shipping :
-    //                   </td>
-    //                   <td
-    //                     colSpan='3'
-    //                     className='price'
-    //                     style={{
-    //                       lineHeight: '49px',
-    //                       textAlign: 'right',
-    //                       paddingRight: '28px',
-    //                       fontSize: '13px',
-    //                       color: '#000000',
-    //                       TtextAlign: 'right',
-    //                       borderLeft: ' unset',
-    //                     }}
-    //                   >
-    //                     <b>&#8377;{singleOrder.shipping}</b>
-    //                   </td>
-    //                 </tr>
-    //                 <tr>
-    //                   <td
-    //                     colSpan='2'
-    //                     style={{
-    //                       lineHeight: '49px',
-    //                       fontSize: '13px',
-    //                       color: '#000000',
-    //                       paddingLeft: '20px',
-    //                       textAlign: 'left',
-    //                       borderRight: 'unset',
-    //                     }}
-    //                   >
-    //                     TOTAL PAID :
-    //                   </td>
-    //                   <td
-    //                     colSpan='3'
-    //                     className='price'
-    //                     style={{
-    //                       lineHeight: '49px',
-    //                       textAlign: 'right',
-    //                       paddingRight: '28px',
-    //                       fontSize: '13px',
-    //                       color: '#000000',
-    //                       TextAlign: 'right',
-    //                       borderLeft: 'unset',
-    //                     }}
-    //                   >
-    //                     <b>&#8377; {singleOrder.payment.amount}</b>
-    //                   </td>
-    //                 </tr>
-    //               </tbody>
-    //             </table>
-    //             <table cellPadding='0' cellSpacing='0' border='0' align='left' style={{ width: '100%', marginTop: '30px', marginBottom: '30px' }}>
-    //               <tbody>
-    //                 <tr>
-    //                   <td style={{ fontSize: '13px', fontWeight: '400', color: '#444444', letterSpacing: '0.2px', width: ' 50%' }}>
-    //                     <h5
-    //                       style={{
-    //                         fontSize: '16px',
-    //                         fontWeight: '500',
-    //                         color: '#000',
-    //                         lineHeight: '16px',
-    //                         paddingBottom: '13px',
-    //                         borderBottom: '1px solid #e6e8eb',
-    //                         letterSpacing: '-0.65px',
-    //                         marginTop: '0',
-    //                         marginBottom: '13px',
-    //                       }}
-    //                     >
-    //                       DELIVERY ADDRESS
-    //                     </h5>
-    //                     <p style={{ textAlign: 'left', fontWeight: 'normal', fontSize: '14px', color: '#000000', lineHeight: '21px', marginTop: '0' }}>
-    //                       {singleOrder.shippingAddress.line1},
-    //                       <br /> {singleOrder.shippingAddress.line2}<br />
-    //                       {`${singleOrder.shippingAddress.state}, ${singleOrder.shippingAddress.country}, ${singleOrder.shippingAddress.pinCode}`}
-    //                     </p>
-    //                   </td>
-    //                   <td width='57' height='25' className='user-info'>
-    //                     <img src={spoce} alt='img' height='25' width='57' />
-    //                   </td>
-    //                   <td className='user-info' style={{ fontSize: '13px', fontWeight: '400', color: '#444444', letterSpacing: '0.2px', width: '50%' }}>
-    //                     <h5
-    //                       style={{
-    //                         fontSize: '16px',
-    //                         fontWeight: '500',
-    //                         color: '#000',
-    //                         lineHeight: '16px',
-    //                         paddingBottom: '13px',
-    //                         borderBottom: '1px solid #e6e8eb',
-    //                         letterSpacing: '-0.65px',
-    //                         marginTop: '0',
-    //                         marginBottom: '13px',
-    //                       }}
-    //                     >
-    //                       SHIPPING ADDRESS
-    //                     </h5>
-    //                     <p style={{ textAlign: 'left', fontWeight: 'normal', fontSize: '14px', color: '#000000', lineHeight: '21px', marginTop: '0' }}>
-    //                       {singleOrder.billingAddress.line1},
-    //                       <br /> {singleOrder.billingAddress.line2}<br />
-    //                       {`${singleOrder.billingAddress.state}, ${singleOrder.billingAddress.country}, ${singleOrder.billingAddress.pinCode}`}
-    //                     </p>
-    //                   </td>
-    //                 </tr>
-    //               </tbody>
-    //             </table>
-    //           </td>
-    //         </tr>
-    //       </div>
-    //     </div>
-    //   </div >
-    // </>
     <>
       <div className='tables_area col'>
         <div className='back_btn_emial'>
@@ -366,11 +25,11 @@ const OrderSuccess = () => {
             <i className='fa fa-arrow-left mr-2'></i>Go Back
           </button>
         </div>
-        <div className='box_table'>
-          <div className='header'>
+        <div className='box_table '>
+          {/* <div className='header'>
             <img src={img1} alt='img' style={{ marginBottom: '30px' }} />
-          </div>
-          <div className='payment-status '>
+          </div> */}
+          <div className='payment-status pt-4'>
             <div className='payment-status-img'>
               <img src={success} alt='img' />
             </div>
@@ -392,7 +51,7 @@ const OrderSuccess = () => {
               </>
             ))}
           </div>
-          <div className='mt-4'>
+          {/* <div className='mt-4'>
             <div className='title'>
               YOUR ORDER DETAILS
             </div>
@@ -499,7 +158,7 @@ const OrderSuccess = () => {
 
             </>
 
-          </div>
+          </div> */}
         </div>
       </div >
     </>
