76fab3d58bfa02c9815c35009f7795f3b4dbe7dc
vimalvysh <vimalvyshagam@gmail.com>
2025-01-30 19:21:40 +0530

    all products filter and sort top

--- src/page/shop/shop-right-sidebar.js
+++ src/page/shop/shop-right-sidebar.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import Header from '../../component/Common/Header'
-import Banner from '../../component/Common/Banner'
-import RightSideBar from '../../component/Shop/RightSideBar'
-import InstgramSlider from '../../component/Common/Instagram'
-import Footer from '../../component/Common/Footer'
+import React from 'react';
+import Header from '../../component/Common/Header';
+import Banner from '../../component/Common/Banner';
+import RightSideBar from '../../component/Shop/RightSideBar';
+import InstgramSlider from '../../component/Common/Instagram';
+import Footer from '../../component/Common/Footer';
 
 const ShopRightSideBar = () => {
-    return (
-        <>
-            <Header />
-             <RightSideBar />
-            <InstgramSlider />
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <RightSideBar />
+      <InstgramSlider />
+      <Footer />
+    </>
+  );
+};
 
-export default ShopRightSideBar
+export default ShopRightSideBar;
